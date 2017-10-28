fff73fe9fe86a88ff4f974094ca0ef8f0203ecc5
Jon Cronin <jonjoncronin@gmail.com>
2017-10-28 15:42:11 -0700

    Build file structure for stack implementation using the llist library

---
+++ stack/src/stack.c
@@ -0,0 +1,14 @@
+/*
+ * stack.c
+ */
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stack/stack.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#ifdef __cplusplus
+}
+#endif
---
+++ stack/test/stack_test.cpp
@@ -0,0 +1,7 @@
+#include <stddef.h>
+#include <gtest/gtest.h>
+#include <stack/stack.h>
+
+TEST(StackTest, HappyPushToEmpty) {
+  ASSERT_EQ(0,0);
+}
