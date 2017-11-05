3829e64f6fa0387f6cccc5dca2de6a744bb1d92b
Jon Cronin <jonjoncronin@gmail.com>
2017-11-04 18:47:02 -0700

    Create skeleton directory structure for sort library

---
+++ sort/include/sort.h
@@ -0,0 +1,16 @@
+/*
+ * sort.h
+ */
+
+#ifndef INCLUDE_SORT_H_
+#define INCLUDE_SORT_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+#ifdef __cplusplus
+}
+#endif
+#endif /* INCLUDE_SORT_H_ */
---
+++ sort/src/sort.c
@@ -0,0 +1,15 @@
+/*
+ * sort.c
+ */
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <sort/sort.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+
+#ifdef __cplusplus
+}
+#endif
