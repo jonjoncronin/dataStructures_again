0a715afd464c2a80175de7ae431b2ff2af6e4574
Jon Cronin <jonjoncronin@gmail.com>
2017-11-05 00:17:07 -0700

    Clean up unit tests for sort tests

--- sort/test/sort_test.cpp
+++ sort/test/sort_test.cpp
@@ -4,78 +4,80 @@
 #include <gtest/gtest.h>
 #include <sort/sort.h>
 
+#define MAX_LIST_SIZE 100000
+
 TEST(SortTest, HappyBubbleSort) {
   clock_t t;
   double elapsed;
   int randomNum;
-  int largeList[10000] = {0};
+  int largeList[MAX_LIST_SIZE] = {0};
 
-  for(int ii = 0; ii < 10000; ii++)
+  for(int ii = 0; ii < MAX_LIST_SIZE; ii++)
   {
     // insert a random number between 1 and 1000
-    largeList[ii] = rand() % 100000 + 1;
+    largeList[ii] = rand() % ((MAX_LIST_SIZE * 10) + 1);
   }
   t = clock();
-  sort_bubble(largeList,10000);
+  sort_bubble(largeList,MAX_LIST_SIZE);
   t = clock() - t;
   elapsed = ((double)t)/CLOCKS_PER_SEC;
   printf("Elapsed %f seconds\n", elapsed);
-  //sort_printArray(largeList, 10000);
+  //sort_printArray(largeList, MAX_LIST_SIZE);
 }
 
 TEST(SortTest, HappySelectionSort) {
   clock_t t;
   double elapsed;
   int randomNum;
-  int largeList[10000] = {0};
+  int largeList[MAX_LIST_SIZE] = {0};
 
-  for(int ii = 0; ii < 10000; ii++)
+  for(int ii = 0; ii < MAX_LIST_SIZE; ii++)
   {
     // insert a random number between 1 and 1000
-    largeList[ii] = rand() % 100000 + 1;
+    largeList[ii] = rand() % ((MAX_LIST_SIZE * 10) + 1);
   }
   t = clock();
-  sort_selection(largeList,10000);
+  sort_selection(largeList,MAX_LIST_SIZE);
   t = clock() - t;
   elapsed = ((double)t)/CLOCKS_PER_SEC;
   printf("Elapsed %f seconds\n", elapsed);
-  // sort_printArray(largeList, 10000);
+  // sort_printArray(largeList, MAX_LIST_SIZE);
 }
 
 TEST(SortTest, HappyMergeSort) {
   clock_t t;
   double elapsed;
   int randomNum;
-  int largeList[10000] = {0};
+  int largeList[MAX_LIST_SIZE] = {0};
 
-  for(int ii = 0; ii < 10000; ii++)
+  for(int ii = 0; ii < MAX_LIST_SIZE; ii++)
   {
     // insert a random number between 1 and 1000
-    largeList[ii] = rand() % 100000 + 1;
+    largeList[ii] = rand() % ((MAX_LIST_SIZE * 10) + 1);
   }
   t = clock();
-  sort_merge(largeList, 0, 9999);
+  sort_merge(largeList, 0, MAX_LIST_SIZE - 1);
   t = clock() - t;
   elapsed = ((double)t)/CLOCKS_PER_SEC;
   printf("Elapsed %f seconds\n", elapsed);
-  // sort_printArray(largeList, 10);
+  // sort_printArray(largeList, MAX_LIST_SIZE);
 }
 
 TEST(SortTest, HappyInsertionSort) {
   clock_t t;
   double elapsed;
   int randomNum;
-  int largeList[10000] = {0};
+  int largeList[MAX_LIST_SIZE] = {0};
 
-  for(int ii = 0; ii < 10000; ii++)
+  for(int ii = 0; ii < MAX_LIST_SIZE; ii++)
   {
     // insert a random number between 1 and 1000
-    largeList[ii] = rand() % 100000 + 1;
+    largeList[ii] = rand() % ((MAX_LIST_SIZE * 10) + 1);
   }
   t = clock();
-  sort_insertion(largeList, 10000);
+  sort_insertion(largeList, MAX_LIST_SIZE);
   t = clock() - t;
   elapsed = ((double)t)/CLOCKS_PER_SEC;
   printf("Elapsed %f seconds\n", elapsed);
-  // sort_printArray(largeList, 10000);
+  // sort_printArray(largeList, MAX_LIST_SIZE);
 }
